d5f633f239c60ee771fc8c7dca91700e8f3a30d8
tjrimonm <tjrimonr@gmail.com>
2022-05-08 23:50:39 +0600

    view single item

--- src/hooks/useViewSingleInventory.js
+++ src/hooks/useViewSingleInventory.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 
-const useViewSingleInventory = itemId =>{
+const useViewSingleInventory = itemId => {
     const [item, setItem] = useState({});
 
-    useEffect( () =>{
+    useEffect(() => {
         const url = `https://thawing-mountain-91486.herokuapp.com/service/${itemId}`;
         console.log(url);
         fetch(url)
-        .then(res=> res.json())
-        .then(data => setItem(data));
+            .then(res => res.json())
+            .then(data => setItem(data));
 
     }, [setItem]);
     return [item]
